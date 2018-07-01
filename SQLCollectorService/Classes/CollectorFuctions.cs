05be3cf80ba320d0ac7e7f95c2414fac08c66a00
Anders Sørensen <anders@galberg.com>
2018-07-02 00:36:11 +0200

    Initial start

---
+++ SQLCollectorService/Classes/CollectorFuctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace SQLCollectorService.Classes
+{
+    class CollectorFuctions
+    {
+        public static int CollectorInit(string ConnectionString, string ServerName)
+        {
+            SqlParameter SQLSourceInstance = new SqlParameter("@ServerName", SqlDbType.VarChar);
+            SQLSourceInstance.Value = ServerName;
+            SqlParameter CollectID = new SqlParameter("@ID", SqlDbType.Int);
+            CollectID.Direction = ParameterDirection.Output;
+            Int32 outCollectID = 0;
+            using (SqlDataReader reader = SqlHelper.ExecuteReader(ConnectionString, "usp_NewCollection", System.Data.CommandType.StoredProcedure, SQLSourceInstance, CollectID))
+            {
+                    outCollectID = (int)CollectID.Value;
+            }
+
+
+            return outCollectID;
+                
+        }
+    }
+}
--- SQLCollectorService/Program.cs
+++ SQLCollectorService/Program.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Management.RegisteredServers;
+using Microsoft.SqlServer.Management.Smo;
+
 
 namespace SQLCollectorService
 {
@@ -10,6 +16,22 @@ namespace SQLCollectorService
     {
         static void Main(string[] args)
         {
+            //RegisteredServerCollection registeredServers = new RegisteredServerCollection; 
+            //System.Data.Sql.SqlDataSourceEnumerator instance = System.Data.Sql.SqlDataSourceEnumerator.Instance;
+
+            string ConnectionString = "Data Source=.; Initial Catalog=IDBMGMT;Integrated Security=true;";
+
+            System.Data.DataTable table2 = SmoApplication.EnumAvailableSqlServers(true);
+
+            foreach (System.Data.DataRow row in table2.Rows)
+            {
+                int CollectID = SQLCollectorService.Classes.CollectorFuctions.CollectorInit(ConnectionString,row["Name"].ToString());
+
+                Console.WriteLine("CollectID for " + row["Name"].ToString() + " is: " + CollectID.ToString());
+            }
+            //EnumAvailableSqlServers
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
         }
     }
 }
